d7257b4de8ff501d2046a4b8eed3064123f412d2
pdev-gd <83932298+pdev-gd@users.noreply.github.com>
2022-06-11 13:15:57 +0900

    client.js

--- client.js
+++ client.js
@@ -14,15 +14,10 @@ redisClient.on("error", function(err) {
 
 // Sets the key "octocat" to a value of "Mona the octocat"
 redisClient.set("octocat", "Mona the Octocat", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
-// Gets all fields in "species" key
+redisClient.set("species1", "Cat and Octopus", redis.print);
+redisClient.set("species2", "Dinosaur and Octopus", redis.print);
 
-redisClient.hkeys("species", function (err, replies) {
+redisClient.keys("species", function (err, replies) {
     console.log(replies.length + " replies:");
     replies.forEach(function (reply, i) {
         console.log("    " + i + ": " + reply);
